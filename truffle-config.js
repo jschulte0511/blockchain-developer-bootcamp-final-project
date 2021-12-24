cf2d906f6e516af6baf473252a6174e9e2a3f6bc
Jurgen <jurgen.schulte@gmail.com>
2021-12-24 20:36:44 +0200

    Added env support

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,31 @@
+
 const path = require("path");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+/**
+ * Use this file to configure your truffle project. It's seeded with some
+ * common settings for different networks and features like migrations,
+ * compilation and testing. Uncomment the ones you need or modify
+ * them to suit your project as necessary.
+ *
+ * More information about configuration can be found at:
+ *
+ * trufflesuite.com/docs/advanced/configuration
+ *
+ * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
+ * to sign your transactions before they're sent to a remote public node. Infura accounts
+ * are available for free at: infura.io/register.
+ *
+ * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
+ * public/private key pairs. If you're publishing your code to GitHub make sure you load this
+ * phrase from a file you've .gitignored so it doesn't accidentally become public.
+ *
+ */
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const mnemonic = process.env.MNEMONIC;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -9,6 +36,18 @@ module.exports = {
       port: 8545
     }
   },
+
+  rinkeby: {
+    provider: () => new HDWalletProvider(mnemonic,
+      `https://ropsten.infura.io/v3/${process.env.ROPSTEN_INFURA_PROJECT_ID}`),
+    network_id: 4,
+    gas: 5500000,
+    confirmations: 2,
+    timeoutBlocks: 200,
+    skipDryRun: true
+  },
+
+
 
   // Configure your compilers
   compilers: {
