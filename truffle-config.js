36aed145220593eb6a7bd3f48579292e2f72f1d4
Jurgen <jurgen.schulte@gmail.com>
2021-12-30 16:45:58 +0200

    Setup for Rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -38,13 +38,12 @@ module.exports = {
 
 
     rinkeby: {
+      // truffle migrate --network rinkeby --reset --compile-all
       provider: () => new HDWalletProvider(mnemonic,
-        `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`),
-      network_id: 3, // Ropsten's id
-      gas: 5500000, // Ropsten has a lower block limit than mainnet
-      confirmations: 0, // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true // Skip dry run before migrations? (default: false for public nets )
+        `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`),
+      network_id: 4
+      , gas: 6700000
+      , gasPrice: 10000000000
     },
   },
 
