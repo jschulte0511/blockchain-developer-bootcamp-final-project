9ac72e0a462c73cfd3d747584325375a50027d69
Jurgen <jurgen.schulte@gmail.com>
2022-01-07 20:11:58 +0200

    Cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
         deployedNetwork && deployedNetwork.address,
       );
 
-      instance.options.address = "0x6E5Aa64824CAaDEaF2Dc1978a53fd52Aa8499C49";
+      instance.options.address = "0x829Cb737C3bcd2c8bfb405B1C07EFe68072d289D";
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
